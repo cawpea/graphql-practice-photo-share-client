f98e677fe756776264ba05f746c255aaa819b914
m.osumi <m.osumi@goodpatch.com>
2023-02-04 15:41:21 +0900

    :+1: show users using graphql-request

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import { request } from "graphql-request";
+
+type User = {
+  name: string;
+  avatar: string;
+};
+
+const apiUrl = "http://localhost:4000/graphql";
+
+const query = `
+query listUsers {
+  allUsers {
+    avatar
+    name
+  }
+}
+`;
+
+const mutation = `
+  mutation populate($count: Int!) {
+    addFakeUsers(count: $count) {
+      githubLogin
+    }
+  }
+`;
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const fetchUsers = async () => {
+    const result = await request(apiUrl, query);
+    console.log("result", result);
+    setUsers(result.allUsers);
+  };
+
+  const addUser = async () => {
+    await request(apiUrl, mutation, { count: 1 });
+    await fetchUsers();
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.length > 0 && (
+        <ul>
+          {users.map((user, index) => (
+            <li key={index}>
+              <img src={user.avatar} alt="" width="40" height="40" />
+              <p>{user.name}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+      <button type="button" onClick={addUser}>
+        Add User
+      </button>
     </div>
   );
 }
