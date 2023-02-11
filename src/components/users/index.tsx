3ed66bb8e65856ebec72e8c36c2bb995da302893
m.osumi <m.osumi@goodpatch.com>
2023-02-11 15:01:10 +0900

    :+1: add button to refetch users

--- src/components/users/index.tsx
+++ src/components/users/index.tsx
@@ -4,7 +4,7 @@ import { UserList } from "./components";
 
 export const Users = () => {
   const result = useQuery(ROOT_QUERY);
-  const { data, loading } = result;
+  const { data, loading, refetch } = result;
   if (!data) {
     return <p>Loading</p>;
   }
@@ -14,6 +14,11 @@ export const Users = () => {
     <div>
       <p>Loading: {loading ? "yes" : "no"}</p>
       {allUsers && allUsers.length > 0 && <UserList users={allUsers} />}
+      <div>
+        <button type="button" onClick={() => refetch()}>
+          Refetch
+        </button>
+      </div>
     </div>
   );
 };
