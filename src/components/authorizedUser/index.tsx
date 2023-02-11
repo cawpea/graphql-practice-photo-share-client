276a59e3f9c27984a7064b6141549649edd462ed
m.osumi <m.osumi@goodpatch.com>
2023-02-11 16:28:28 +0900

    :+1: save token to local storage of GitHub OAuth

--- src/components/authorizedUser/index.tsx
+++ src/components/authorizedUser/index.tsx
@@ -1,7 +1,28 @@
 import { FC, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useMutation } from "react-apollo";
+import { gql } from "apollo-boost";
+import { ROOT_QUERY } from "../../App";
+
+const GITHUB_AUTH_MUTATION = gql`
+  mutation githubAuth($code: String!) {
+    githubAuth(code: $code) {
+      token
+    }
+  }
+`;
 
 export const AuthorizedUser: FC = () => {
+  const navigate = useNavigate();
   const [isSigningIn, setSigningIn] = useState<boolean>(false);
+  const [githubAuthMutation] = useMutation(GITHUB_AUTH_MUTATION, {
+    update: (cache, { data }) => {
+      console.log("update", data);
+      localStorage.setItem(`github-auth-token`, data.githubAuth.token);
+      setSigningIn(true);
+    },
+    refetchQueries: [{ query: ROOT_QUERY }],
+  });
 
   const requestCode = () => {
     const clientId = "0ed0924c4c2512563c17";
@@ -12,9 +33,10 @@ export const AuthorizedUser: FC = () => {
     if (window.location.search.match(/code=/)) {
       setSigningIn(true);
       const code = window.location.search.replace("?code=", "");
-      alert(code);
+      navigate("/", { replace: true });
+      githubAuthMutation({ variables: { code } });
     }
-  }, []);
+  }, [githubAuthMutation, navigate]);
 
   return (
     <button type="button" onClick={() => requestCode()} disabled={isSigningIn}>
