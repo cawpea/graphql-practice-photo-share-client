ccf5d879011898f6e1f0d8f9a0703f43d84046bc
m.osumi <m.osumi@goodpatch.com>
2023-02-11 15:26:22 +0900

    :+1: add button to call mutation to add fake user

--- src/components/users/index.tsx
+++ src/components/users/index.tsx
@@ -1,24 +1,46 @@
-import { useQuery } from "react-apollo";
+import { useQuery, useMutation } from "react-apollo";
+import { gql } from "apollo-boost";
 import { ROOT_QUERY } from "../../App";
 import { UserList } from "./components";
 
+const ADD_FAKE_USERS_MUTATION = gql`
+  mutation addFakeUsers($count: Int!) {
+    addFakeUsers(count: $count) {
+      githubLogin
+      name
+      avatar
+    }
+  }
+`;
+
 export const Users = () => {
-  const result = useQuery(ROOT_QUERY);
-  const { data, loading, refetch } = result;
+  const { data, loading, refetch } = useQuery(ROOT_QUERY);
+  const [addFakeUserMutation] = useMutation(ADD_FAKE_USERS_MUTATION, {
+    refetchQueries: [{ query: ROOT_QUERY }],
+  });
+
   if (!data) {
     return <p>Loading</p>;
   }
   const { allUsers } = data;
 
   return (
-    <div>
-      <p>Loading: {loading ? "yes" : "no"}</p>
-      {allUsers && allUsers.length > 0 && <UserList users={allUsers} />}
+    <>
+      <div>
+        <p>Loading: {loading ? "yes" : "no"}</p>
+        {allUsers && allUsers.length > 0 && <UserList users={allUsers} />}
+      </div>
       <div>
         <button type="button" onClick={() => refetch()}>
-          Refetch
+          Refetch Users
+        </button>
+        <button
+          type="button"
+          onClick={() => addFakeUserMutation({ variables: { count: 1 } })}
+        >
+          Add Fake User
         </button>
       </div>
-    </div>
+    </>
   );
 };
